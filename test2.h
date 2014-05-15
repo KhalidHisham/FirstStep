dfbbf67ab2099050735340c35c5b1a486b84d508
KhalidHisham <khalid.h.obaid@gmail.com>
2014-05-15 11:02:16 +0200

    file addition
    Adding new file to repository from GUI

---
+++ test2.h
@@ -0,0 +1,40 @@
+/*!
+ @file    test.h
+
+ @details
+ @b DESCRIPTION
+ This is a test header file 
+
+ @b DEPENDENCIES
+ @n None
+
+ @n @Copyright (C) 2014 
+*******************************************************************************/
+#ifndef TEST_H
+#define TEST_H
+
+/*!
+@name EEPROM Address Specification
+@{
+*******************************************************************************/
+#define USER_INIT_ADDR      0x00
+#define HIGH_VOLT_ADDR      0x08
+#define HIGH_CURR_ADDR      0x09
+#define HIGH_LIMT_ADDR      0x10
+
+/*! @}*************************************************************************/
+
+/*!
+@name Default Values 
+@{
+*******************************************************************************/
+#define USER_INIT_VAL      0x10
+#define HIGH_VOLT_VAL      0xAA
+#define HIGH_CURR_VAL      0x66
+#define HIGH_LIMT_VAL      0x48
+/*! @}*************************************************************************/
+
+#endif 
+/*
+ *  End of File
+ ******************************************************************************/
