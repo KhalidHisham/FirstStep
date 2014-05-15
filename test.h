01220bb8c151585dd350962b6490d63a2af7730c
KhalidHisham <khalid.h.obaid@gmail.com>
2014-05-15 10:28:21 +0200

    Added first file

---
+++ test.h
@@ -0,0 +1,38 @@
+/*!
+ @file    test.h
+
+ @details
+ @b DESCRIPTION
+ This is a test header file 
+
+ @b DEPENDENCIES
+ @n None
+
+ @n @Copyright (C) 2014 
+*******************************************************************************/
+#ifndef TEST_H
+#define TEST_H
+
+/*!
+@name EEPROM Address Specification
+@{
+*******************************************************************************/
+#define USER_INIT_ADDR      0x00
+#define HIGH_VOLT_ADDR      0x01
+#define HIGH_CURR_ADDR      0x02
+
+/*! @}*************************************************************************/
+
+/*!
+@name Default Values 
+@{
+*******************************************************************************/
+#define USER_INIT_VAL      0x10
+#define HIGH_VOLT_VAL      0x1A
+#define HIGH_CURR_VAL      0xF0
+/*! @}*************************************************************************/
+
+#endif 
+/*
+ *  End of File
+ ******************************************************************************/
