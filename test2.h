12c6e8c43220d61e1e41e86f88c6695e9fa6e9a8
KhalidHisham <khalid.h.obaid@gmail.com>
2014-05-15 11:09:11 +0200

    Revert "new test commit from GUI"
    This reverts commit f26b8074f359773e30fc6117a01fcfff3631dfb2.
    
    Conflicts:
    	test2.h

--- test2.h
+++ test2.h
@@ -29,9 +29,15 @@
 @{
 *******************************************************************************/
 #define USER_INIT_VAL      0x10
+<<<<<<< HEAD:test2.h
 #define HIGH_VOLT_VAL      0xAA
 #define HIGH_CURR_VAL      0x66
 #define HIGH_LIMT_VAL      0x48
+=======
+#define HIGH_VOLT_VAL      0x1A
+#define HIGH_CURR_VAL      0x22
+#define HIGH_LIMT_VAL      0xB2
+>>>>>>> parent of f26b807... new test commit from GUI:test.h
 /*! @}*************************************************************************/
 
 #endif 
